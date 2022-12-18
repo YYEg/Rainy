246e41a1eff3f5afe901fde5dbd0cc27fc17641c
Egor <yyrkovskii@mail.ru>
2022-12-18 21:25:42 +0800

    emitter and a half of gravity

--- WindowsFormsApp1/Emitter.cs
+++ WindowsFormsApp1/Emitter.cs
@@ -4,20 +4,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WindowsFormsApp1.Particle;
 
 namespace WindowsFormsApp1
 {
     internal class Emitter
     {
         List<Particle> particles = new List<Particle>();
+        public List<Point> gravityPoints = new List<Point>();
+
         public int MousePositionX;
         public int MousePositionY;
+
+        public float GravitationX = 0;
+        public float GravitationY = 1;
 
         public void UpdateState()
         {
             foreach (var particle in particles)
             {
-                
                 particle.Life -= 1;
 
                 if (particle.Life < 0)
@@ -37,8 +42,40 @@ namespace WindowsFormsApp1
                 }
                 else
                 {
+                    float gX = gravityPoints[0].X - particle.X;
+                    float gY = gravityPoints[0].Y - particle.Y;
+
+                    
+                    float r2 = gX * gX + gY * gY;
+                    float M = 100; // сила притяжения к точке
+
+                    
+                    particle.SpeedX += (gX) * M / r2;
+                    particle.SpeedY += (gY) * M / r2;
+
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
                     particle.X += particle.SpeedX;
                     particle.Y += particle.SpeedY;
+                }
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.PowderBlue;
+                    particle.ToColor = Color.FromArgb(0, Color.LightSteelBlue);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
                 }
             }
         }
@@ -48,6 +85,17 @@ namespace WindowsFormsApp1
             foreach (var particle in particles)
             {
                 particle.Draw(g);
+            }
+
+            foreach (var point in gravityPoints)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
             }
         }
     }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,79 +15,27 @@ namespace WindowsFormsApp1
     {
 
         List<Particle> particles = new List<Particle>(); //список частиц
+        Emitter emitter = new Emitter();
 
         public Form1()
         {
             InitializeComponent();
 
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height); //привязали изображение
+
+            emitter.gravityPoints.Add(new Point(
+            picDisplay.Width / 2, picDisplay.Height / 2
+            ));
         }
-
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1; 
-
-                if (particle.Life < 0)
-                {
-                    
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-
-                    particle.Radius = 2 + Particle.rand.Next(5);
-                }
-                else
-                {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-        }
-
-        private void Render(Graphics g)
-        {
-            
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500) 
-                {
-                    var particle = new ParticleColorful();
-                    
-                    particle.FromColor = Color.PowderBlue;
-                    particle.ToColor = Color.FromArgb(0, Color.LightSteelBlue);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
         
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black);
-                Render(g);
+                emitter.Render(g);
             }
 
             picDisplay.Invalidate();
@@ -98,8 +46,8 @@ namespace WindowsFormsApp1
 
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
