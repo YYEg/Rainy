dca48f1b09ea86a9835b80c8c816a55e95e29c54
Egor <yyrkovskii@mail.ru>
2022-12-19 00:22:37 +0800

    im starting to do my radar

--- WindowsFormsApp1/IImpactPoint.cs
+++ WindowsFormsApp1/IImpactPoint.cs
@@ -12,11 +12,8 @@ namespace WindowsFormsApp1
         public float X; // ну точка же, вот и две координаты
         public float Y;
 
-        // абстрактный метод с помощью которого будем изменять состояние частиц
-        // например притягивать
         public abstract void ImpactParticle(Particle particle);
 
-        // базовый класс для отрисовки точечки
         public void Render(Graphics g)
         {
             g.FillEllipse(
